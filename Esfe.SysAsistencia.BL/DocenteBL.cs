1d4db9d40b68aa45444f56244eada97d83f1d635
Eliseo Arévalo <2480931@clases.edu.sv>
2023-03-29 04:44:21 -0600

    [:sparkles: update BL] lógica de negocio

---
+++ Esfe.SysAsistencia.BL/DocenteBL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Esfe.SysAsistencia.EN;
+using Esfe.SysAsistencia.DAL;
+
+namespace Esfe.SysAsistencia.BL
+{
+    public class DocenteBL
+    {
+        private DocenteDAL docenteDAL = new DocenteDAL();
+
+        public void AgregarDocente(Docente docente)
+        {
+            docenteDAL.AgregarDocente(docente);
+        }
+
+        public List<Docente> ObtenerDocentes()
+        {
+            return docenteDAL.ObtenerDocentes();
+        }
+
+        public bool ExisteDocente(Docente buscar)
+        {
+            return docenteDAL.ExisteDocente(buscar);
+        }
+    }
+}
--- Esfe.SysAsistencia.BL/EstudianteBL.cs
+++ Esfe.SysAsistencia.BL/EstudianteBL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Esfe.SysAsistencia.DAL;
+using Esfe.SysAsistencia.EN;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,21 @@ namespace Esfe.SysAsistencia.BL
 {
     public class EstudianteBL
     {
+        private EstudianteDAL estudianteDAL = new EstudianteDAL();
+
+        public void AgregarEstudiante(Estudiante estudiante)
+        {
+            estudianteDAL.AgregarEstudiante(estudiante);
+        }
+
+        public List<Estudiante> ObtenerEstudiante()
+        {
+            return estudianteDAL.ObtenerEstudiantes();
+        }
+
+        public bool ExisteEstudiatne(Estudiante estudiante)
+        {
+            return estudianteDAL.ExisteEstudiante(estudiante);
+        }
     }
 }
