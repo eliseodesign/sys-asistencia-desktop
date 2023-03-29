0f00bc478273091cd64aebcc50784b8dc56c7379
Eliseo Arévalo <2480931@clases.edu.sv>
2023-03-29 04:43:55 -0600

    [:sparkles: update DAL] lógica de acceso a datos

---
+++ Esfe.SysAsistencia.DAL/DocenteDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Esfe.SysAsistencia.EN;
+
+namespace Esfe.SysAsistencia.DAL
+{
+    public class DocenteDAL
+    {
+        private List<Docente> Docentes = new List<Docente>();
+
+        public void AgregarDocente(Docente docente)
+        {
+            docente.Id = Docentes.Select(d => d.Id).DefaultIfEmpty(0).Max() + 1;
+            Docentes.Add(docente);
+        }
+
+        public List<Docente> ObtenerDocentes()
+        {
+            return Docentes;
+        }
+
+        public bool ExisteDocente(Docente buscar)
+        {
+            return Docentes.Exists(docente => docente.Id == buscar.Id);
+        }
+    }
+}
--- Esfe.SysAsistencia.DAL/EstudianteDAL.cs
+++ Esfe.SysAsistencia.DAL/EstudianteDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Esfe.SysAsistencia.EN;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,22 @@ namespace Esfe.SysAsistencia.DAL
 {
     public class EstudianteDAL
     {
+        private List<Estudiante> Estudiantes = new List<Estudiante>();
+
+        public void AgregarEstudiante(Estudiante estudiante)
+        {
+            estudiante.Id = Estudiantes.Select(d => d.Id).DefaultIfEmpty(0).Max() + 1;
+            Estudiantes.Add(estudiante);
+        }
+
+        public List<Estudiante> ObtenerEstudiantes()
+        {
+            return Estudiantes;
+        }
+
+        public bool ExisteEstudiante(Estudiante buscarE)
+        {
+            return Estudiantes.Exists(e => e.Id == buscarE.Id);
+        }
     }
 }
