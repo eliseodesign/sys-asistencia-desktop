aa05f009e421e0ab802404472cf7ef0bd701be2e
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-05-14 08:21:49 -0600

    [:bug: fix] change properties grupo

---
+++ Esfe.SysAsistencia.DAL/ComunBD.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//-------------------------------
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ESFE.BDConn.DAL
+{
+    public class ComunBD
+    {
+        //---Se coloca el link de conexion en una constante
+        const string StrConexion = @"Data Source=localhost;Initial Catalog=BDTienda;Integrated Security=True";
+        //----Establecemos la conexion con SQL
+        private static SqlConnection ObtenerConexion()
+        {
+            // obtenemos la conexión y la abrimos
+            SqlConnection connection = new SqlConnection(StrConexion);
+            connection.Open();
+            return connection;
+        }
+        public static SqlCommand ObtenerComando()
+        {
+            // obtener comando
+            SqlCommand command = new SqlCommand();
+            command.Connection = ObtenerConexion();
+            return command;
+        }
+        public static int EjecutarComando(SqlCommand pComando)
+        {
+            int resultado = pComando.ExecuteNonQuery();
+            pComando.Connection.Close();
+            return resultado;
+        }
+        public static SqlDataReader EjecutarReader(SqlCommand pComando)
+        {
+            SqlDataReader reader = pComando.ExecuteReader(CommandBehavior.CloseConnection);
+            return reader;
+        }
+
+
+        //Ejecutar Comando
+        public static int EjecutarSP(string nombreSP, object objeto, List<string> parametros)
+        {
+            SqlCommand comando = ComunBD.ObtenerComando();
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.CommandText = nombreSP;
+            foreach (var propiedad in objeto.GetType().GetProperties())
+            {
+                if (parametros.Contains(propiedad.Name))
+                {
+                    var valor = propiedad.GetValue(objeto, null);
+                    comando.Parameters.AddWithValue("@" + propiedad.Name, valor);
+                }
+            }
+            return EjecutarComando(comando);
+        }
+        public static List<T> EjecutarSPSelect<T>(string nombreSP) where T : new()
+        {
+            SqlCommand comando = ComunBD.ObtenerComando();
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.CommandText = nombreSP;
+
+            SqlDataReader reader = ComunBD.EjecutarReader(comando);
+
+            List<T> lista = new List<T>();
+            while (reader.Read())
+            {
+                T item = new T();
+                foreach (var propiedad in item.GetType().GetProperties())
+                {
+                    if (reader[propiedad.Name] != DBNull.Value)
+                    {
+                        propiedad.SetValue(item, reader[propiedad.Name], null);
+                    }
+                }
+                lista.Add(item);
+            }
+            reader.Close();
+            return lista;
+        }
+
+
+    }
+}
--- Esfe.SysAsistencia.EN/Grupo.cs
+++ Esfe.SysAsistencia.EN/Grupo.cs
@@ -13,35 +13,10 @@ namespace Esfe.SysAsistencia.EN
     {
         public int Id { get; set; }
         public byte EstudiantesMax { get; set; }
-        public bool[] DiasPresencial { get; set; } = new bool[5];
+        public string DiasPresencial { get; set; } 
         //public int DocenteId { get; set; } = 0;
-        public string Codigo { get; set; }
-        public string Carrera { get; set; }
-        public string Turno { get; set; }
-        public string Docente { get; set; }
-        
-        public string Presencial
-        {
-            get
-            {
-                string[] diasSemana = new string[] { "Lun", "Mar", "Mie", "Jue", "Vie" };
-                string diasPresenciales = "";
-
-                for (int i = 0; i < DiasPresencial.Length; i++)
-                {
-                    if (DiasPresencial[i])
-                    {
-                        diasPresenciales += ", " + diasSemana[i];
-                    }
-                }
-
-                if (diasPresenciales.Length > 0)
-                {
-                    return diasPresenciales = diasPresenciales.Substring(1);
-                }
-                return diasPresenciales.Trim();
-            }
-        }
-
+        public byte IdNumGrupo { get; set; }
+        public byte IdCarrera { get; set; }
+        public byte IdTurno { get; set; }
     }
 }
