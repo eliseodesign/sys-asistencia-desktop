cb8be35e6e9f4a94075c2a99f44004870716c04c
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-05-15 20:30:24 -0600

    [:sparkles: add] en dal and bl of GrupoDocente table

---
+++ Esfe.SysAsistencia.EN/GrupoDocente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esfe.SysAsistencia.EN
+{
+    public class GrupoDocente
+    {
+        public int Id { get; set; }
+        public byte IdGrupo {get;set;}
+        public int IdDocente {get;set; }
+    }
+}
